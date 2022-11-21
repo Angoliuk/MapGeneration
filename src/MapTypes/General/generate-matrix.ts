8a238cb991db9980b41f724d15c0666329b1a415
Angoliuk <angoliukstas@gmail.com>
2022-11-21 10:05:34 +0200

    added generation tile func

--- src/MapTypes/General/generate-matrix.ts
+++ src/MapTypes/General/generate-matrix.ts
@@ -1,24 +1,20 @@
-import { mapSize } from '../../configs';
-import { getNoise } from './get-noise';
-import { getType } from './get-type';
-import { colors } from './noise-colors';
-import { noiseConfigs } from './noise-configs';
+import { mapSize } from '../../World/configs';
+import { generateTile } from './generate-tile';
 import { NoiseNames } from './noise-names';
 
 export interface generateMatrixProps {
   noiseName: NoiseNames;
 }
-
+/**
+ * @deprecated
+ */
 export const generateMatrix = ({ noiseName }: generateMatrixProps): Array<Array<string>> => {
   const pixels: string[][] = [];
   for (let y = 0; y < mapSize.height; y++) {
     pixels.push([]);
     for (let x = 0; x < mapSize.width; x++) {
-      const noise = getNoise({ ...noiseConfigs[noiseName], x, y });
-      const type = getType(noise, noiseName);
-      const u = 1 - noise;
-      // @ts-expect-error types in colors
-      pixels[y][x] = `rgb(${ colors[noiseName][type].from.R * u + colors[noiseName][type].to.R * noise }, ${ colors[noiseName][type].from.G * u + colors[noiseName][type].to.G * noise }, ${ colors[noiseName][type].from.B * u + colors[noiseName][type].to.B * noise })`;
+      const tile = generateTile({ noiseName, x, y });
+      pixels[y][x] = tile.color;
     }
   }
   return pixels;
