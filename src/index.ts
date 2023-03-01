e36a073c0f1dad8125831fc4c06c899d639a61c6
Angoliuk <gamongara@gmail.com>
2023-03-01 16:47:09 +0200

    main file refactored

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,51 @@
-import { generateMapCanvas } from './Map/canvas-map-generator';
-import { drawTile } from './Map/draw-tile';
 import './styles.css';
-import { pointSize, timeConfigs } from './World/configs';
-import { generateWorld } from './World/generate-world';
 
-setInterval(() => {
-  timeConfigs.date = timeConfigs.date.plus({ days: 1 });
-  const timer = document.getElementById('timer');
-  timer.textContent = timeConfigs.date.toLocaleString({ year: 'numeric', weekday: 'short', month: 'short', day: '2-digit' });
-}, timeConfigs.dayTime);
+import { timer } from './components/timer/timer';
+import { pointSize, timeConfigs } from './constants/world';
+import { generateMapCanvas } from './map/canvas-map-generator';
+import { drawTile } from './map/draw-tile';
+import { generateWorld } from './modules/world/world-generator';
+import { WorldTile } from './modules/world/world-tiles';
+
+class World {
+  constructor() {
+    this._world = generateWorld();
+  }
+
+  _world: {
+    world: WorldTile[][];
+  };
+
+  get world() {
+    return this._world;
+  }
+}
+
+const generatedWorld = new World();
+
+const moveDayTime = () => {
+  const mapContext = (document.querySelector('#light') as HTMLCanvasElement).getContext('2d');
+  if (!mapContext) return;
+  const { world } = generatedWorld.world;
+  for (const worldRow of world) {
+    for (const worldTile of worldRow) {
+      worldTile.changeLightOn(0.2);
+      drawTile({
+        color: worldTile.light.color,
+        mapContext,
+        x: worldTile.x,
+        y: worldTile.y,
+      });
+    }
+  }
+  setTimeout(() => requestAnimationFrame(moveDayTime), timeConfigs.dayTime);
+};
 
 const createControls = () => {
   const biggerScaleButton = document.createElement('button');
   biggerScaleButton.textContent = '+';
   biggerScaleButton.classList.add('scale-button');
-  biggerScaleButton.onclick = () => {
+  biggerScaleButton.addEventListener('click', () => {
     biggerScaleButton.disabled = true;
     biggerScaleButton.textContent = 'wait';
     setTimeout(() => {
@@ -24,7 +55,7 @@ const createControls = () => {
       biggerScaleButton.disabled = false;
       biggerScaleButton.textContent = '+';
     }, 0);
-  };
+  });
 
   const scaleText = document.createElement('p');
   scaleText.classList.add('scale-text');
@@ -33,7 +64,7 @@ const createControls = () => {
   const smallerScaleButton = document.createElement('button');
   smallerScaleButton.textContent = '-';
   smallerScaleButton.classList.add('scale-button');
-  smallerScaleButton.onclick = () => {
+  smallerScaleButton.addEventListener('click', () => {
     smallerScaleButton.disabled = true;
     smallerScaleButton.textContent = 'wait';
     setTimeout(() => {
@@ -43,11 +74,7 @@ const createControls = () => {
       smallerScaleButton.disabled = false;
       smallerScaleButton.textContent = '-';
     }, 0);
-  };
-
-  const timer = document.createElement('p');
-  timer.id = 'timer';
-  timer.textContent = timeConfigs.date.toLocaleString({ year: 'numeric', weekday: 'short', month: 'short', day: '2-digit' });
+  });
 
   const scaleControlsWrapper = document.createElement('div');
   scaleControlsWrapper.classList.add('scale-button-wrapper');
@@ -59,36 +86,19 @@ const createControls = () => {
   scaleControlsWrapper.prepend(scaleText);
   scaleControlsWrapper.prepend(smallerScaleButton);
   navBar.prepend(scaleControlsWrapper);
-  navBar.prepend(timer);
+  navBar.prepend(timer());
   document.body.prepend(navBar);
 };
 
-const { light, world } = generateWorld();
-
 const drawWorld = () => {
+  const { world } = generatedWorld.world;
   generateMapCanvas({ tilesMatrix: world, mapType: 'climate' });
   generateMapCanvas({ tilesMatrix: world, mapType: 'humidity' });
   generateMapCanvas({ tilesMatrix: world, mapType: 'relief' });
-  generateMapCanvas({ tilesMatrix: light, mapType: 'light' });
-  const moveDayTime = () => {
-    const mapContext = (document.getElementById('light') as HTMLCanvasElement).getContext('2d');
-    for (const lightRow of light) {
-      for (const lightTile of lightRow) {
-        lightTile.changeLightOn(0.2);
-        drawTile({
-          color: lightTile.light.color,
-          mapContext,
-          x: lightTile.x,
-          y: lightTile.y
-        });
-      }
-    }
-    setTimeout(() => requestAnimationFrame(moveDayTime), timeConfigs.dayTime);
-  };
-  requestAnimationFrame(moveDayTime);
-
+  generateMapCanvas({ tilesMatrix: world, mapType: 'light' });
   generateMapCanvas({ tilesMatrix: world, mapType: 'biome' });
 };
 
 drawWorld();
 createControls();
+requestAnimationFrame(moveDayTime);
